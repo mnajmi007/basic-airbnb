aebf3096ed93854bdacd6d32f06540d66da0f801
unknown <muhammadnajmi824@gmail.com>
2022-06-17 23:30:24 +0700

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroImage from "../images/hero.png";
+
+function Hero(){
+    return(
+        <div>
+            <img src={HeroImage} />
+        </div>
+    )
+}
+
+export default Hero;
